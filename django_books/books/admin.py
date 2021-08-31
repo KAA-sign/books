ed2ad6a5ef64509c6790418ed38aada9080d34f2
KAA-sign <q89308033587@gmail.com>
2021-08-31 16:36:54 +0300

    models+admin_2

--- django_books/books/admin.py
+++ django_books/books/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Author, Genre, Book, TextBook, RatingStar, Rating, Reviews
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(TextBook)
+admin.site.register(RatingStar)
+admin.site.register(Rating)
+admin.site.register(Reviews)
--- django_books/django_books/settings.py
+++ django_books/django_books/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
