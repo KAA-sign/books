87d5ab2406f8b162f3061be6a0f450434527ebf7
KAA-sign <q89308033587@gmail.com>
2021-09-07 16:36:22 +0300

    add_book_detail_view

--- django_books/books/urls.py
+++ django_books/books/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.BookView.as_view())
+    path('', views.BookView.as_view()),
+    path("<int:pk>", views.BookDetailView.as_view())
 ]
--- django_books/books/views.py
+++ django_books/books/views.py
@@ -10,4 +10,12 @@ class BookView(View):
     '''Спссок книг'''
     def get(self, request):
         books = Book.objects.all()
-        return render(request, 'books/book_list.html', {'book_list': books})
+        return render(request, 'books/books.html', {'book_list': books})
+
+
+class BookDetailView(View):
+    '''Полное описание книги'''
+    def get(self, request, pk):
+        book = Book.objects.get(id=pk)
+        return render(request, 'books/book_detail.html', {'book': book})
+
